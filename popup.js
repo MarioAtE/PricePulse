f1e6a195051c63afdfd4532a4b9e73d5fe3f23f4
Mario @ E <142985691+MarioAtE@users.noreply.github.com>
2023-08-29 08:59:34 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.sync.get(['refreshRate', 'amount', 'siteUrl'], (data) => {
+  chrome.storage.sync.get(['refreshRate', 'amount', 'site'], (data) => {
     document.getElementById('refreshRate').value = data.refreshRate || 1;
     document.getElementById('amount').value = data.amount || 'R349';
-    document.getElementById('siteUrl').value = data.siteUrl || 'https://www.example.com/';
+    document.getElementById('site').value = data.site || '';
   });
 
   document.getElementById('save').addEventListener('click', () => {
     const refreshRate = document.getElementById('refreshRate').value;
     const amount = document.getElementById('amount').value;
-    const siteUrl = document.getElementById('siteUrl').value;
+    const site = document.getElementById('site').value;
 
-    chrome.storage.sync.set({ refreshRate, amount, siteUrl }, () => {
+    chrome.storage.sync.set({ refreshRate, amount, site }, () => {
       alert('Settings saved successfully.');
-      chrome.action.setBadgeText({text: 'SET'});
+      chrome.alarms.clear('refreshPage', () => {
+        chrome.alarms.create('refreshPage', {
+          delayInMinutes: parseFloat(refreshRate),
+          periodInMinutes: parseFloat(refreshRate)
+        });
+      });
     });
   });
 });
