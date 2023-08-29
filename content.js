6d6ad7ba8172dda7cd604d619f1fe7e27067d0fb
Mario @ E <142985691+MarioAtE@users.noreply.github.com>
2023-08-29 08:36:40 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Fetch user-defined settings
   chrome.storage.sync.get(['amount'], (data) => {
     const specificPrice = data.amount || 'R349';
-    const regex = new RegExp(`\\b${specificPrice}\\b`, 'g');
-
-    // Scan the entire page text for the specific price
+    const regex = new RegExp(`\\b${specificPrice}(\\.00)?(,00)?\\b`, 'g');
     const bodyText = document.body.innerText;
     const matches = bodyText.match(regex);
-
-    // Action to take if the price is found
     if (matches && matches.length > 0) {
-      alert(`Price found: ${specificPrice}`);
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'Price Found',
+        message: `Price found: ${specificPrice}`
+      });
       chrome.runtime.sendMessage({command: "clearAlarm"});
     }
-    // If not found, the page will be refreshed based on the background.js alarm
   });
 });
