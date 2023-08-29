8205673cf20b756332e067940e5c63587688f479
Mario @ E <142985691+MarioAtE@users.noreply.github.com>
2023-08-29 09:00:09 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,21 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.sync.get(['amount'], (data) => {
-    const specificPrice = data.amount || 'R349';
-    const regex = new RegExp(`\\b${specificPrice}\\b`, 'g');
-    const bodyText = document.body.innerText;
-    const matches = bodyText.match(regex);
+  chrome.storage.sync.get(['amount', 'site'], (data) => {
+    const specificWordOrAmount = data.amount || 'R349';
+    const site = data.site || '';
 
-    if (matches && matches.length > 0) {
-      chrome.notifications.create({
-        type: 'basic',
-        iconUrl: 'icon.png',
-        title: 'Price Found',
-        message: `Price found: ${specificPrice}`
-      });
-      chrome.action.setBadgeText({text: ''});
-      chrome.runtime.sendMessage({command: "clearAlarm"});
-    } else {
-      chrome.action.setBadgeText({text: 'NF'});
+    if (window.location.href.startsWith(site)) {
+      const regex = new RegExp(`\\b${specificWordOrAmount}\\b`, 'g');
+      const bodyText = document.body.innerText;
+      const matches = bodyText.match(regex);
+
+      if (matches && matches.length > 0) {
+        alert(`Word/Amount found: ${specificWordOrAmount}`);
+        chrome.runtime.sendMessage({command: "clearAlarm"});
+      }
     }
   });
 });
