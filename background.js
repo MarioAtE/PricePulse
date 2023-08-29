d03087ecf4747b9e40cfcf3af5729eaa4bb14ada
Mario @ E <142985691+MarioAtE@users.noreply.github.com>
2023-08-29 08:37:00 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,33 +1,27 @@
-// Create an alarm once the extension is installed
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.get(['refreshRate'], (data) => {
     const rate = parseFloat(data.refreshRate) || 1;
-    chrome.alarms.create('refreshPage', {
-      delayInMinutes: rate,
-      periodInMinutes: rate
+    chrome.alarms.get('refreshPage', (alarm) => {
+      if (!alarm) {
+        chrome.alarms.create('refreshPage', {
+          delayInMinutes: rate,
+          periodInMinutes: rate
+        });
+      }
     });
   });
 });
 
-// Listen for alarm and refresh the page
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'refreshPage') {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      if (tabs[0]) {
+  chrome.storage.sync.get(['siteUrl'], (data) => {
+    const siteUrl = data.siteUrl || 'https://www.example.com/';
+    chrome.tabs.query({ url: siteUrl }, (tabs) => {
+      if (tabs.length > 0) {
         chrome.scripting.executeScript({
           target: {tabId: tabs[0].id},
-          function: () => {
-            location.reload();
-          }
+          files: ['content.js']
         });
       }
     });
-  }
-});
-
-// Listen for messages from content.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.command === 'clearAlarm') {
-    chrome.alarms.clear('refreshPage');
-  }
+  });
 });
