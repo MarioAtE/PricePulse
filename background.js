8c0c4cfaed7748603e63d9ca359f749e089f5be4
Mario @ E <142985691+MarioAtE@users.noreply.github.com>
2023-08-29 09:28:14 +0200

    Update background.js

--- background.js
+++ background.js
@@ -4,14 +4,19 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ word: '', refreshRate: 5 });
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.active) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab, changes) => {
+  if (changes.manifest_version === 3 && changeInfo.status === 'complete' && tab.active) {
     // Inject code to scan the page content
     chrome.tabs.executeScript(tabId, {
       code: `document.body.innerText.includes("${word}")`
     }, (results) => {
       if (results && results[0]) {
-        alert('Word/Amount found.');
+        chrome.notifications.create({
+          title: 'Word/Amount found!',
+          message: 'The word or amount you are looking for has been found on this page.',
+          icon: 'icon.png',
+          type: 'basic'
+        });
       } else {
         alert('Scanning started.');
         setTimeout(() => {
